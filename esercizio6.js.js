83c58344cb1dc7cae573b45cb4041c4e0c4f062f
Mattia Pietro Martolini <martolinimattiapietro@gmail.com>
2023-11-10 19:17:46 +0100

    Esercizio6

---
+++ esercizio6.js.js
@@ -0,0 +1,90 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* - String: utilizzata per elementi di tipo stringa (Ex: 'stringa');
+- Number: utilizzata per elementi di tipo numero (Ex: 1, 2, 3.5);
+- Boolean: utilizzata per valori true o false;
+- Object: utilizzato per racchiudere un insieme di valori;
+- Null;
+- Symbol;
+- Undefined; */
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* Un oggetto è un elemento che serve pèr racchiudere un insieme di variabili, anche di tipo diverso. 
+Può racchiudere un insieme di informazioni per esempio di uno studente (nome, cognome, età, scuola...) */
+
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* 
+let n1 = 12;
+let n1 = 20;
+let risultato = n1 + n2;
+
+ */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* let x = 12; */
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* let name = "mattia"; */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+
+/* let risultato = 12+20;
+let x = 12;
+risultato -= x;
+console.log(risultato); */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+
+/* 
+let name1 = "john";
+let name2 = "John";
+
+console.log(name1 !== name2);
+console.log(name1.toLowerCase() === name2.toLowerCase()); */
+
+/*ESERCIZIO 8*/
+/* let persona = {
+    nome: "mattia",
+    cognome: "martolini",
+    eta: 22,
+    indirizzo: {
+        via: "via del sole",
+        civico: 9,
+        citta: "milano",
+    }
+};
+
+console.log(persona); */
